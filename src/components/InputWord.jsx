0115de1f7f9af2df2473fb278fa2f1d754bad787
sk-nahid <sknahid0111@gmail.com>
2025-08-30 22:50:07 +0600

    project done

--- src/components/InputWord.jsx
+++ src/components/InputWord.jsx
@@ -6,9 +6,13 @@ const InputWord = () => {
     const [playerTwo, setPlayerTwo] = useState([])
     const [allWord, setAllWord] = useState([])
     const [error, setError] = useState('')
+    const [errorTwo, setErrorTwo] = useState('')
     const [firstLetter, setFirstLetter] = useState('')
-    const [playerOneBlock, setPlayerOneBlock]= useState(false)
-    const [playerTwoBlock, setPlayerTwoBlock]= useState(false)
+    const [playerOneBlock, setPlayerOneBlock] = useState(false)
+    const [playerTwoBlock, setPlayerTwoBlock] = useState(false)
+    const [plOneScore, setPlOneScore] = useState(0)
+    const [plTwoScore, setPlTwoScore] = useState(0)
+    const [yourTurn, setYourTurn] = useState('plOne')
 
     console.log(playerOne)
 
@@ -18,7 +22,14 @@ const InputWord = () => {
         const word = getWord.toLowerCase()
         const data = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         const res = await data
-
+        setPlayerOneBlock(true)
+        setPlayerTwoBlock(false)
+        const startLetter = word.slice(0, 1)
+        console.log(startLetter)
+        if (firstLetter && startLetter !== firstLetter) {
+            setError('start with the given letter')
+            return
+        }
         if (!res.ok) {
             setError('use a valid word')
             return
@@ -37,41 +48,98 @@ const InputWord = () => {
         }
         setError('')
         const nextLet = word.slice(-1)
-        console.log(nextLet)
         setFirstLetter(nextLet)
+
+
         setPlayerOne([...playerOne, word])
-        setPlayerOneBlock(true)
-        setPlayerTwoBlock(false)
-        console.log(word)
+
+        setPlOneScore(plOneScore + 10);
+
+        setYourTurn('plTwo')
+
+
     }
 
 
 
-    const handleFormSubmitTwo = (e) => {
-        e.preventDefault();
-        const word = e.target.playerTwo.value;
-        console.log(word)
+    const handleFormSubmitTwo = async (e) => {
+        e.preventDefault()
+        const getWord = e.target.playerTwo.value;
+        const word = getWord.toLowerCase()
+        const data = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        const res = await data
         setPlayerTwoBlock(true)
         setPlayerOneBlock(false)
+
+        const startLetter = word.slice(0, 1)
+        console.log(startLetter)
+        if (firstLetter && startLetter !== firstLetter) {
+            setErrorTwo('start with the given letter')
+            return
+        }
+        if (!res.ok) {
+            setErrorTwo('use a valid word')
+            return
+        }
+
+        if (word.length <= 3) {
+            setErrorTwo('word must have 4 character')
+            return
+        }
+
+        setAllWord([...allWord, word])
+        const findWord = allWord.find(w => w === word)
+        if (findWord) {
+            setError('duplicate word is not allowed')
+            return
+        }
+        setErrorTwo('')
+        const nextLet = word.slice(-1)
+        setFirstLetter(nextLet)
+
+
+        setPlayerTwo([...playerTwo, word])
+
+        setPlTwoScore(plTwoScore + 10);
+
+        setYourTurn('plOne')
+
+        e.target.playerOne.value = 'h'
+
+
     }
 
     return (
         <div className='max-w-7xl mx-auto grid grid-cols-2'>
+
             <div>
+                <h2>make word with <span className='text-2xl font-bold text-amber-400'>{firstLetter}</span></h2>
+                <h4>score: {plOneScore}</h4>
+                {yourTurn === "plOne" && <h4 className='text-green-500'>Your Turn</h4>}
                 <form onSubmit={handleFormSubmitOne}>
                     <h1 className='text-xl font-bold '>Player 1</h1>
-                    <input type="text" placeholder="Type here" name='playerOne' defaultValue={firstLetter} className="input" disabled={playerOneBlock}  />
+                    <input type="text" placeholder="Type here" name='playerOne' defaultValue={firstLetter} className="input" disabled={playerOneBlock} />
                     {error && <p className='text-red-500'>{error}</p>}
                 </form>
                 <ul>
-                    {playerOne.map(w => <li className='bullet'>{ w}</li>)}
+                    {playerOne.map(w => <li className='bullet'>{w}</li>)}
                 </ul>
             </div>
-            <form onSubmit={handleFormSubmitTwo}>
-                <h1 className='text-xl font-bold '>Player 1</h1>
-                <input type="text" placeholder="Type here" name='playerTwo' defaultValue={firstLetter} className="input" disabled={playerTwoBlock}  />
-            </form>
+            <div>
+                <form onSubmit={handleFormSubmitTwo}>
+                    <h2>make word with <span className='text-2xl font-bold text-amber-400'>{firstLetter}</span></h2>
 
+                    <h4>score: {plTwoScore}</h4>
+                    {yourTurn === "plTwo" && <h4 className='text-green-500'>Your Turn</h4>}
+                    <h1 className='text-xl font-bold '>Player 1</h1>
+                    <input type="text" placeholder="Type here" name='playerTwo' defaultValue={firstLetter} className="input" disabled={playerTwoBlock} />
+                    {errorTwo && <p className='text-red-500'>{errorTwo}</p>}
+                </form>
+                <ul>
+                    {playerTwo.map(w => <li className='bullet'>{w}</li>)}
+
+                </ul>
+            </div>
         </div>
     );
 };
